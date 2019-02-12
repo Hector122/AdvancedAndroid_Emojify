292f4ec0d2bd7310a738bece481f178eeb0e274a
hcastillo122 <hector122@gmail.com>
2019-02-12 19:37:56 -0400

    Add detect faces method

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -2,10 +2,29 @@ package com.example.android.emojify;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 public class Emojifier {
 
-    static void detectFaces(Context contex, Bitmap bitmap){
+    static void detectFaces(Context context, Bitmap bitmap){
+        //crating a frame instance
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
 
+
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        //Calling the detector synchronously
+        SparseArray<Face> faces = detector.detect(frame);
+
+        Log.i("Faces -----------", String.valueOf(faces.size()));
     }
 }
