8263545f608e5561181670976fcd70d14937988f
Hector Castillo (ClaroDom) <hector castillo@MAC-5fj82.local>
2019-02-13 09:00:10 -0400

    Add release method

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.util.SparseArray;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Frame;
@@ -13,18 +14,28 @@ import com.google.android.gms.vision.face.FaceDetector;
 public class Emojifier {
 
     static void detectFaces(Context context, Bitmap bitmap){
-        //crating a frame instance
-        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-
-
+        // instance a face detector
         FaceDetector detector = new FaceDetector.Builder(context)
                 .setTrackingEnabled(false)
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .build();
 
+
+        //crating a frame instance
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+
         //Calling the detector synchronously
         SparseArray<Face> faces = detector.detect(frame);
 
-        Log.i("detectFaces", "Number of faces = " + String.valueOf(faces.size()));
+        Log.i("Emojifier", "--------- detectFaces() Number of faces = " + faces.size());
+
+        // If there are no faces detected, show a Toast message
+        if(faces.size() == 0){
+            Toast.makeText(context, "no_faces_message found", Toast.LENGTH_SHORT).show();
+        }
+
+
+        detector.release();
     }
 }
